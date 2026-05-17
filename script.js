d415641c5f8bf4d4a8aae91e8a4840fe59cd5577
Pepetotrueno <vmarambio46@gmail.com>
2026-05-17 15:03:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 console.log("¡Conectado!")
 document.body.style.backgroundColor="write";// Cambiar el fondo de cuerpo negro mediante Js
-const nuevaImagen = document.createElement("img");//Creamos la "caja" de la imagen
-nuevaImagen.src = "https://sandovallegacygroup.com/wp-content/uploads/2025/08/Screenshot-2025-08-14-at-9.19.56-AM-854x675.pngLA";
-nuevaImagen.alt = "Jeffrey Epstein (1953-2019) fue un financiero estadounidense que pasó de ser profesor y operador en Wall Street a convertirse en un multimillonario. Es conocido a nivel mundial por dirigir una red internacional de tráfico sexual de menores y abusar de decenas de niñas en sus propiedades y en su isla privada en el Caribe";//Recomendado para accesibilidad
-document.body.appendChild(nuevaImagen);
+
