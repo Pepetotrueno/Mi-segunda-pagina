baff82b8d267c77f580dd7468746b9423578466d
Pepetotrueno <vmarambio46@gmail.com>
2026-05-17 13:52:28 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("¡Conectado!")
+document.body.style.backgroundColor="write";// Cambiar el fondo de cuerpo negro mediante Js
+nuevaImagen.alt = "Jeffrey Epstein (1953-2019) fue un financiero estadounidense que pasó de ser profesor y operador en Wall Street a convertirse en un multimillonario. Es conocido a nivel mundial por dirigir una red internacional de tráfico sexual de menores y abusar de decenas de niñas en sus propiedades y en su isla privada en el Caribe";//Recomendado para accesibilidad
